e93f25ba6e1d5f26cb96e8e62dde8b8b36da479b
bukinoshita <bukinoshita@gmail.com>
2019-06-14 21:22:30 -0300

    refactor: home page

--- pages/index.js
+++ pages/index.js
@@ -1,89 +1,63 @@
 // Packages
 import React, { Component } from 'react'
-import { autoBind } from 'react-extras'
-import { encode } from 'base64-arraybuffer'
-import Router from 'next/router'
+import Link from 'next/link'
+import { withRouter } from 'next/router'
+import dynamic from 'next/dynamic'
+import { Choose } from 'react-extras'
 
 // Layouts
 import Page from '../layouts/page'
 
-// UI
-import Textarea from '../ui/textarea'
-import Button from '../ui/button'
-import theme from '../ui/theme'
+// Components
+import Introduction from '../components/introduction'
 
-// Services
-import api from '../services/api'
-import { encrypt } from '../services/crypto'
+const WebContent = dynamic({ loader: () => import('../components/web') })
+const CliContent = dynamic({ loader: () => import('../components/cli') })
+const SlackContent = dynamic({ loader: () => import('../components/slack') })
 
 class Home extends Component {
-  constructor() {
-    super()
-
-    autoBind(this)
-
-    this.state = { secret: '' }
-  }
-
-  async onFormSubmit(e) {
-    e.preventDefault()
-
-    const { secret } = this.state
-
-    try {
-      const crypto = await encrypt(secret)
-      const ctBuffer = encode(crypto.ctBuffer)
-      const iv = encode(crypto.iv)
-      const { uid } = await api.post('/canary', { ctBuffer, iv })
-
-      Router.push(`/secret?uid=${uid}`)
-    } catch (error) {
-      console.error({ error })
-    }
-  }
-
-  onSecretChange(e) {
-    const { target } = e
-    const { value } = target
-
-    this.setState({ secret: value })
-  }
-
   render() {
-    const { secret } = this.state
+    const { router } = this.props
 
     return (
       <Page>
-        <h1>
-          Secret helps you to send a message through a safe, private, and encrypted link that
-          <br />
-          automatically expires to ensure your stuff does not remain online forever
-        </h1>
+        <main>
+          <Introduction />
 
-        <form onSubmit={this.onFormSubmit}>
-          <Textarea
-            name="secret"
-            label="Create your secret"
-            placeholder="Type your secret here"
-            value={secret}
-            onChange={this.onSecretChange}
-          />
+          <section className="product">
+            <Choose>
+              <Choose.When condition={router.query.content === 'cli'}>
+                <CliContent />
+              </Choose.When>
 
-          <Button type="submit">Create secret</Button>
-        </form>
+              <Choose.When condition={router.query.content === 'slack'}>
+                <SlackContent />
+              </Choose.When>
 
-        <style jsx>{`
-          h1 {
-            font-size: ${theme.typography.title.fontSize};
-            font-weight: ${theme.typography.title.fontWeight};
-            line-height: ${theme.typography.title.lineHeight};
-            text-align: center;
-            margin-bottom: ${theme.spacing.xxxLarge};
-          }
-        `}</style>
+              <Choose.Otherwise>
+                <WebContent />
+              </Choose.Otherwise>
+            </Choose>
+          </section>
+
+          <style jsx>{`
+            main {
+              display: flex;
+              justify-content: space-between;
+              min-height: 100vh;
+            }
+
+            .product {
+              flex-basis: 50%;
+              width: 50%;
+              background-color: #fff;
+              padding: 100px 48px;
+            }
+          `}</style>
+        </main>
       </Page>
     )
   }
 }
 
-export default Home
+export default withRouter(Home)
